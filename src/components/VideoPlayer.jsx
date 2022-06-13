6d5ea52e0d60fb745491a4dbeb52e4c46ba5408d
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-13 14:19:38 -0300

    created video player component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -5,7 +5,6 @@ export default function VideoPlayer(){
     <div>
     <video id="videoPlayer" width="650" controls='controls' muted="muted" autoplay>
     <source src="http://localhost:3500/movie" type="video/mp4" />
-    <source src="http://localhost:3500/movie" type="video/webm" />
     <track label="Español" kind="subtitles" srclang="es" src="REV_TRANSCRIPTION_FINAL.es.vtt"></track>
     <track label="English" kind="subtitles" srclang="en-us" src="./src/components/rashomonen.en-us.vtt" />
   </video>
